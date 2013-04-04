a47d872c26b5cb8f3f5408b83b718cfe97877877
Christoph Burgmer <cburgmer@ira.uka.de>
2013-04-05 00:48:45 +0200

    Update to rasterizeHTML.js 0.4.0 with fixes & performance improvements

--- dist/csscritic.js
+++ dist/csscritic.js
@@ -1,4 +1,4 @@
-/*! CSS critic - v0.1.0 - 2013-02-24
+/*! CSS critic - v0.1.0 - 2013-04-05
 * http://www.github.com/cburgmer/csscritic
 * Copyright (c) 2013 Christoph Burgmer, Copyright (c) 2012 ThoughtWorks, Inc.; Licensed MIT */
 
