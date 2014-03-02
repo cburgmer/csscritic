6dc741d1bf0575ad3247b7ada41a70f1cd2a5162
Christoph Burgmer <cburgmer@ira.uka.de>
2014-03-02 21:49:29 +0100

    Bump version

--- dist/csscritic-phantom.js
+++ dist/csscritic-phantom.js
@@ -1,4 +1,4 @@
-/*! PhantomJS regression runner for CSS Critic - v0.1.0 - 2014-02-20
+/*! PhantomJS regression runner for CSS Critic - v0.2.0 - 2014-03-02
 * http://www.github.com/cburgmer/csscritic
 * Copyright (c) 2014 Christoph Burgmer, Copyright (c) 2012 ThoughtWorks, Inc.; Licensed  */
 /* Integrated dependencies:
