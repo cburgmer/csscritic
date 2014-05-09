9b9db1eb2672aa3b6954008aa65078623f3cb2de
Christoph Burgmer <cburgmer@ira.uka.de>
2014-05-09 17:33:03 +0200

    Move cli deps into one source

--- test/run-csscritic-phantom.js
+++ test/run-csscritic-phantom.js
@@ -2,19 +2,5 @@
 // This version references all dependencies and can be used to test against the latest changes.
 "use strict";
 
-phantom.injectJs("node_modules/imagediff/imagediff.js");
-phantom.injectJs("node_modules/jssha/src/sha.js");
-phantom.injectJs("node_modules/ayepromise/ayepromise.js");
-phantom.injectJs("build/dependencies/inlineresources.js");
-phantom.injectJs("src/boot/scope.js");
-phantom.injectJs("src/cli/phantomjsbind.js");
-phantom.injectJs("src/cli/phantomjsrenderer.js");
-phantom.injectJs("src/cli/signoffreporterutil.js");
-phantom.injectJs("src/cli/signoffreporter.js");
-phantom.injectJs("src/cli/terminalreporter.js");
-phantom.injectJs("src/cli/htmlfilereporter.js");
-phantom.injectJs("src/cli/filestorage.js");
-phantom.injectJs("src/cli/phantomjs_runner.js");
-phantom.injectJs("src/util.js");
-phantom.injectJs("src/csscritic.js");
+phantom.injectJs("test/csscritic-cli.js");
 phantom.injectJs("src/boot/cli.js");
