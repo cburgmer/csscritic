80df87102c537bcc733470c045c8c88a4bab2d62
Christoph Burgmer <cburgmer@ira.uka.de>
2014-05-10 22:41:56 +0200

    Pull out tests

---
+++ test/specs/MainSpec.js
@@ -0,0 +1,201 @@
+describe("Main", function () {
+    "use strict";
+
+    var csscritic, regression, reporting;
+
+    var util = csscriticLib.util();
+
+    var reporter;
+
+    var setUpComparison = function (comparison) {
+        regression.compare.and.returnValue(testHelper.successfulPromise(comparison));
+    };
+
+    beforeEach(function () {
+        reporting = jasmine.createSpyObj('reporting', ['doReportComparisonStarting', 'doReportComparison', 'doReportTestSuite']);
+        reporting.doReportComparisonStarting.and.returnValue(testHelper.successfulPromise());
+        reporting.doReportComparison.and.returnValue(testHelper.successfulPromise());
+        reporting.doReportTestSuite.and.returnValue(testHelper.successfulPromise());
+
+        regression = jasmine.createSpyObj('regression', ['compare']);
+
+        csscritic = csscriticLib.main(
+            regression,
+            reporting,
+            util);
+
+        reporter = {};
+    });
+
+    describe("adding & executing", function () {
+        it("should return on an empty list of tests", function (done) {
+            csscritic.execute(function (passed) {
+                expect(passed).toBeTruthy();
+
+                done();
+            });
+        });
+
+        it("should handle a missing callback", function () {
+            csscritic.execute();
+        });
+
+        it("should execute regression test", function () {
+            setUpComparison();
+
+            csscritic.add("test_case");
+            csscritic.execute(function () {
+                expect(regression.compare).toHaveBeenCalledWith({
+                    url: "test_case"
+                });
+            });
+        });
+    });
+
+    describe("on a passing comparison", function () {
+        beforeEach(function () {
+            setUpComparison({
+                status: "passed"
+            });
+        });
+
+        it("should result in success", function (done) {
+            csscritic.add("samplepage.html");
+
+            csscritic.execute(function (success) {
+                expect(success).toBeTruthy();
+
+                done();
+            });
+        });
+
+        it("should report overall success in the test suite", function (done) {
+            csscritic.addReporter(reporter);
+            csscritic.add("succeedingpage.html");
+            csscritic.execute(function () {
+                expect(reporting.doReportTestSuite).toHaveBeenCalledWith([reporter], true);
+
+                done();
+            });
+        });
+    });
+
+    describe("on a failing comparison", function () {
+        beforeEach(function () {
+            setUpComparison({
+                status: "failed"
+            });
+        });
+
+        it("should result in failure", function (done) {
+            csscritic.add("samplepage.html");
+
+            csscritic.execute(function (passed) {
+                expect(passed).toBeFalsy();
+
+                done();
+            });
+        });
+
+        it("should report a failure in the test suite", function (done) {
+            csscritic.addReporter(reporter);
+            csscritic.add("failingpage.html");
+            csscritic.execute(function () {
+                expect(reporting.doReportTestSuite).toHaveBeenCalledWith([reporter], false);
+
+                done();
+            });
+        });
+    });
+
+    describe("Reporting", function () {
+        var triggerDelayedPromise = function () {
+            jasmine.clock().tick(100);
+        };
+
+        beforeEach(function () {
+            jasmine.clock().install();
+        });
+
+        afterEach(function() {
+            jasmine.clock().uninstall();
+        });
+
+        beforeEach(function () {
+            csscritic.addReporter(reporter);
+        });
+
+        it("should report a starting comparison", function () {
+            csscritic.add("samplepage.html");
+            csscritic.execute();
+
+            expect(reporting.doReportComparisonStarting).toHaveBeenCalledWith([reporter], [{
+                url: "samplepage.html"
+            }]);
+        });
+
+        it("should wait for reporting on starting comparison to finish", function () {
+            var defer = ayepromise.defer(),
+                callback = jasmine.createSpy('callback');
+
+            reporting.doReportComparisonStarting.and.returnValue(defer.promise);
+            csscritic.execute(callback);
+
+            triggerDelayedPromise();
+            expect(callback).not.toHaveBeenCalled();
+
+            defer.resolve();
+
+            triggerDelayedPromise();
+            expect(callback).toHaveBeenCalled();
+        });
+
+        it("should call final report on empty test case list and report as successful", function (done) {
+            csscritic.execute(function () {
+                expect(reporting.doReportTestSuite).toHaveBeenCalledWith([reporter], true);
+
+                done();
+            });
+
+            triggerDelayedPromise();
+        });
+
+        it("should wait for reporting on comparison to finish", function () {
+            var defer = ayepromise.defer(),
+                callback = jasmine.createSpy('callback');
+
+            setUpComparison({
+                status: "success"
+            });
+
+            reporting.doReportComparison.and.returnValue(defer.promise);
+            csscritic.add('a_test');
+            csscritic.execute(callback);
+
+            triggerDelayedPromise();
+            expect(callback).not.toHaveBeenCalled();
+
+            defer.resolve();
+
+            triggerDelayedPromise();
+            expect(callback).toHaveBeenCalled();
+        });
+
+        it("should wait for reporting on final report to finish", function () {
+            var defer = ayepromise.defer(),
+                callback = jasmine.createSpy('callback');
+
+            reporting.doReportTestSuite.and.returnValue(defer.promise);
+            csscritic.execute(callback);
+
+            triggerDelayedPromise();
+            expect(callback).not.toHaveBeenCalled();
+
+            defer.resolve();
+
+            triggerDelayedPromise();
+            expect(callback).toHaveBeenCalled();
+        });
+
+    });
+});
--- test/specs/RegressionSpec.js
+++ test/specs/RegressionSpec.js
@@ -1,7 +1,7 @@
 describe("Regression testing", function () {
     "use strict";
 
-    var csscritic, regression, rendererBackend, storageBackend, reporting, imagediff;
+    var regression, rendererBackend, storageBackend, imagediff;
 
     var util = csscriticLib.util();
 
@@ -51,36 +51,14 @@ describe("Regression testing", function () {
         rendererBackend = jasmine.createSpyObj('renderer', ['render']);
         imagediff = jasmine.createSpyObj('imagediff', ['diff', 'equal']);
 
-        reporting = jasmine.createSpyObj('reporting', ['doReportComparisonStarting', 'doReportComparison', 'doReportTestSuite']);
-        reporting.doReportComparisonStarting.and.returnValue(testHelper.successfulPromise());
-        reporting.doReportComparison.and.returnValue(testHelper.successfulPromise());
-        reporting.doReportTestSuite.and.returnValue(testHelper.successfulPromise());
-
         regression = csscriticLib.regression(rendererBackend, storageBackend, util, imagediff);
-
-        csscritic = csscriticLib.main(
-            regression,
-            reporting,
-            util);
     });
 
-    describe("adding & executing", function () {
+    describe("comparison", function () {
         beforeEach(function () {
             setUpRenderedImage(htmlImage);
             setUpReferenceImage(referenceImage, viewport);
             setUpImageEqualityToBe(true);
-        });
-
-        it("should return on an empty list of tests", function (done) {
-            csscritic.execute(function (passed) {
-                expect(passed).toBeTruthy();
-
-                done();
-            });
-        });
-
-        it("should handle a missing callback", function () {
-            csscritic.execute();
         });
 
         it("should compare the rendered page against the reference image", function (done) {
@@ -101,16 +79,6 @@ describe("Regression testing", function () {
                 }));
 
                 done();
-            });
-        });
-
-        it("should execute regression test", function () {
-            spyOn(regression, 'compare').and.returnValue(testHelper.successfulPromise());
-            csscritic.add("test_case");
-            csscritic.execute(function () {
-                expect(regression.compare).toHaveBeenCalledWith({
-                    url: "test_case"
-                });
             });
         });
 
@@ -141,19 +109,7 @@ describe("Regression testing", function () {
             setUpImageEqualityToBe(true);
         });
 
-        it("should report success", function (done) {
-            csscritic.add("samplepage.html");
-
-            csscritic.execute(function (passed) {
-                expect(imagediff.equal).toHaveBeenCalledWith(htmlImage, referenceImage);
-
-                expect(passed).toBeTruthy();
-
-                done();
-            });
-        });
-
-        it("should report a successful comparison", function (done) {
+        it("should report the comparison", function (done) {
             regression.compare({url: "differentpage.html"}).then(function (comparison) {
                 expect(comparison).toEqual({
                     status: "passed",
@@ -182,15 +138,6 @@ describe("Regression testing", function () {
                 done();
             });
         });
-
-        it("should report overall success in the test suite", function (done) {
-            csscritic.add("succeedingpage.html");
-            csscritic.execute(function () {
-                expect(reporting.doReportTestSuite).toHaveBeenCalledWith(jasmine.any(Object), true);
-
-                done();
-            });
-        });
     });
 
     describe("on a failing comparison", function () {
@@ -200,19 +147,7 @@ describe("Regression testing", function () {
             setUpImageEqualityToBe(false);
         });
 
-        it("should report failure", function (done) {
-            csscritic.add("samplepage.html");
-
-            csscritic.execute(function (passed) {
-                expect(imagediff.equal).toHaveBeenCalledWith(htmlImage, referenceImage);
-
-                expect(passed).toBeFalsy();
-
-                done();
-            });
-        });
-
-        it("should report a failing comparison", function (done) {
+        it("should report the comparison", function (done) {
             regression.compare({url: "differentpage.html"}).then(function (comparison) {
                 expect(comparison).toEqual({
                     status: "failed",
@@ -229,15 +164,6 @@ describe("Regression testing", function () {
                 done();
             });
         });
-
-        it("should report a failure in the test suite", function (done) {
-            csscritic.add("failingpage.html");
-            csscritic.execute(function () {
-                expect(reporting.doReportTestSuite).toHaveBeenCalledWith(jasmine.any(Object), false);
-
-                done();
-            });
-        });
     });
 
     describe("on a reference missing", function () {
@@ -246,29 +172,7 @@ describe("Regression testing", function () {
             setUpReferenceImageToBeMissing();
         });
 
-        it("should report failure", function (done) {
-            csscritic.add({url: "samplepage.html"});
-            csscritic.execute(function (passed) {
-                expect(passed).toBe(false);
-                expect(imagediff.equal).not.toHaveBeenCalled();
-
-                done();
-            });
-        });
-
-        it("should provide a appropriately sized page rendering", function (done) {
-            regression.compare({url: "differentpage.html"}).then(function () {
-                expect(rendererBackend.render).toHaveBeenCalledWith(jasmine.objectContaining({
-                    url: "differentpage.html",
-                    width: 800,
-                    height: 100
-                }));
-
-                done();
-            });
-        });
-
-        it("should report a missing reference image", function (done) {
+        it("should report the comparison", function (done) {
             regression.compare({url: "differentpage.html"}).then(function (comparison) {
                 expect(comparison).toEqual({
                     status: "referenceMissing",
@@ -281,6 +185,18 @@ describe("Regression testing", function () {
                     viewportWidth: jasmine.any(Number),
                     viewportHeight: jasmine.any(Number)
                 });
+
+                done();
+            });
+        });
+
+        it("should provide a appropriately sized page rendering", function (done) {
+            regression.compare({url: "differentpage.html"}).then(function () {
+                expect(rendererBackend.render).toHaveBeenCalledWith(jasmine.objectContaining({
+                    url: "differentpage.html",
+                    width: 800,
+                    height: 100
+                }));
 
                 done();
             });
@@ -304,18 +220,6 @@ describe("Regression testing", function () {
             rendererBackend.render.and.returnValue(testHelper.failedPromise());
         });
 
-        it("should report failure", function (done) {
-            setUpReferenceImageToBeMissing();
-
-            csscritic.add({url: "samplepage.html"});
-            csscritic.execute(function (passed) {
-                expect(passed).toBe(false);
-                expect(imagediff.equal).not.toHaveBeenCalled();
-
-                done();
-            });
-        });
-
         it("should report the comparison", function (done) {
             setUpReferenceImageToBeMissing();
 
@@ -330,98 +234,5 @@ describe("Regression testing", function () {
                 done();
             });
         });
-    });
-
-    describe("Reporting", function () {
-        var reporter;
-
-        var triggerDelayedPromise = function () {
-            jasmine.clock().tick(100);
-        };
-
-        beforeEach(function () {
-            jasmine.clock().install();
-        });
-
-        afterEach(function() {
-            jasmine.clock().uninstall();
-        });
-
-        beforeEach(function () {
-            reporter = {};
-            csscritic.addReporter(reporter);
-        });
-
-        it("should report a starting comparison", function () {
-            csscritic.add("samplepage.html");
-            csscritic.execute();
-
-            expect(reporting.doReportComparisonStarting).toHaveBeenCalledWith([reporter], [{
-                url: "samplepage.html"
-            }]);
-        });
-
-        it("should wait for reporting on starting comparison to finish", function () {
-            var defer = ayepromise.defer(),
-                callback = jasmine.createSpy('callback');
-
-            reporting.doReportComparisonStarting.and.returnValue(defer.promise);
-            csscritic.execute(callback);
-
-            triggerDelayedPromise();
-            expect(callback).not.toHaveBeenCalled();
-
-            defer.resolve();
-
-            triggerDelayedPromise();
-            expect(callback).toHaveBeenCalled();
-        });
-
-        it("should call final report on empty test case list and report as successful", function (done) {
-            csscritic.execute(function () {
-                expect(reporting.doReportTestSuite).toHaveBeenCalledWith([reporter], true);
-
-                done();
-            });
-
-            triggerDelayedPromise();
-        });
-
-        it("should wait for reporting on comparison to finish", function () {
-            var defer = ayepromise.defer(),
-                callback = jasmine.createSpy('callback');
-
-            setUpRenderedImage(htmlImage);
-            setUpReferenceImage(referenceImage, viewport);
-
-            reporting.doReportComparison.and.returnValue(defer.promise);
-            csscritic.add('a_test');
-            csscritic.execute(callback);
-
-            triggerDelayedPromise();
-            expect(callback).not.toHaveBeenCalled();
-
-            defer.resolve();
-
-            triggerDelayedPromise();
-            expect(callback).toHaveBeenCalled();
-        });
-
-        it("should wait for reporting on final report to finish", function () {
-            var defer = ayepromise.defer(),
-                callback = jasmine.createSpy('callback');
-
-            reporting.doReportTestSuite.and.returnValue(defer.promise);
-            csscritic.execute(callback);
-
-            triggerDelayedPromise();
-            expect(callback).not.toHaveBeenCalled();
-
-            defer.resolve();
-
-            triggerDelayedPromise();
-            expect(callback).toHaveBeenCalled();
-        });
-
     });
 });
