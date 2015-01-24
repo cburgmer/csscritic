b14d204020aae312f10611b14ec21acb302feb15
Christoph Burgmer <christoph.burgmer@gmail.com>
2015-01-24 18:22:07 +0100

    Refactor smoke test, pull asserts and flow control out of page under test

--- test/smokeTest.js
+++ test/smokeTest.js
@@ -16,60 +16,37 @@ if (system.args.length !== 2) {
 csscriticLoadingPage = system.args[1];
 
 
+var waitFor = function (truthyTest) {
+    var maxPolls = 20,
+        sleepDuration = 100,
+        iterationCount = 0,
+        defer = ayepromise.defer();
 
-var serializeFunctions = function (funcList) {
-    return '[' + funcList.map(function (f) { return '' + f; }) + ']';
-};
-
-var allOf = function (functions, arg) {
-
-    var executeFunc = function (serializedFunctions, param) {
-        // deserialize functions
-        var funcList = eval(serializedFunctions);
-
-        var workOnList = function () {
-            if (funcList.length === 0) {
-                return param;
-            }
-            var func = funcList.shift();
-            var ret = func(param);
-
-            return ret.then(function (newParam) {
-                return workOnList(funcList, newParam);
-            });
-        };
-
-        workOnList();
-    };
-
-    var serializedFunctions = serializeFunctions(functions);
-
-    return {
-        func: executeFunc,
-        params: [serializedFunctions, arg]
-    };
-};
-
-var loadPageAndEvaluate = function (url, evaluateList) {
-    var page = require('webpage').create(),
-        defer = ayepromise.defer(),
-        initialNavigationFired = false;
-
-    // resolve either on navigation or on explicit callback
-    page.onNavigationRequested = function (url) {
-        if (!initialNavigationFired) {
-            initialNavigationFired = true;
-            return;
+    var poll = function () {
+        var successful = truthyTest();
+        if (successful) {
+            defer.resolve();
+        } else if (iterationCount === maxPolls) {
+            defer.reject();
+        } else {
+            iterationCount += 1;
+            setTimeout(function () {
+                poll();
+            }, sleepDuration);
         }
-        defer.resolve({
-            url: url
-        });
     };
-    page.onCallback = function () {
-        defer.resolve({
-            url: page.url
-        });
-    };
+
+    setTimeout(function () {
+        poll();
+    }, 100);
+
+    return defer.promise;
+};
+
+var loadPage = function (url) {
+    var page = require('webpage').create(),
+        defer = ayepromise.defer();
+
     // Provide some inspection
     page.onConsoleMessage = function (msg) {
         console.log(msg);
@@ -79,72 +56,77 @@ var loadPageAndEvaluate = function (url, evaluateList) {
     };
     // Open page
     page.open(url, function (status) {
-        var evaluate = allOf(evaluateList);
-
         if (status !== 'success') {
             defer.reject('Unable to load the address!');
         } else {
-            page.evaluate.apply(page, [evaluate.func].concat(evaluate.params));
-
-            // "Watchdog", exit eventually & fail if above breaks
-            window.setTimeout(function () {
-                defer.reject('Page did not respond in time');
-            }, 2000);
+            defer.resolve(page);
         }
     });
 
     return defer.promise;
 };
 
-var executeRegressionTest = function () {
-    csscritic.addReporter(csscritic.NiceReporter());
-    // Don't care about the examples
-    csscritic.add('pageThatDoesNotExist');
-    csscritic.add('yetAnotherPageThatDoesNotExist');
-    return csscritic.execute();
-};
-
 var selectASingleTestCase = function () {
-    console.log("Selecting first comparison");
     document.querySelector('#pageThatDoesNotExist .titleLink').click();
 };
 
-var assertOneComparisonHasRun = function () {
-    var comparisonCount = document.querySelectorAll('.comparison').length;
-
-    console.log("Found " + comparisonCount + " comparison(s)");
-    if (comparisonCount === 1) {
-        window.callPhantom();
-    }
+var getComparisonCount = function () {
+    return document.querySelectorAll('.comparison').length;
 };
 
 var runAll = function () {
-    console.log('Clicking "Run all"');
     document.querySelector('.runAll').click();
 };
 
-var assertTwoComparisonsHaveRun = function () {
-    var comparisonCount = document.querySelectorAll('.comparison').length;
-
-    console.log("Found " + comparisonCount + " comparison(s)");
-    if (comparisonCount === 2) {
-        window.callPhantom();
-    }
-};
-
-var reloadAnd = function () {
-    var funcs = Array.prototype.slice.call(arguments, 0);
-
-    return function (result) {
-        return loadPageAndEvaluate(result.url, [executeRegressionTest].concat(funcs));
+var regressionTestToExecute = function (page) {
+    return function () {
+        return page.evaluate(function () {
+            return document.querySelector('#header.fail');
+        });
     };
 };
 
+var assertEquals = function (value, expectedValue, manualMsg) {
+    if (value === expectedValue) {
+        console.log(manualMsg + ": '" + value + "' ✓");
+        return;
+    } else {
+        throw new Error(manualMsg + ": '" + expectedValue + "' but found '" + value +"'");
+    }
+};
 
-loadPageAndEvaluate(fs.absolute(csscriticLoadingPage), [executeRegressionTest, selectASingleTestCase])
-    .then(reloadAnd(assertOneComparisonHasRun))
-    .then(reloadAnd(runAll))
-    .then(reloadAnd(assertTwoComparisonsHaveRun))
+
+var page;
+
+loadPage(fs.absolute(csscriticLoadingPage))
+    .then(function (thePage) {
+        page = thePage;
+
+        console.log("Waiting for regression test to finish executing");
+        return waitFor(regressionTestToExecute(page));
+    })
+    .then(function () {
+        console.log("Selecting first comparison");
+
+        page.evaluate(selectASingleTestCase);
+        return waitFor(regressionTestToExecute(page));
+    })
+    .then(function () {
+        var comparisonCount = page.evaluate(getComparisonCount);
+
+        assertEquals(comparisonCount, 1, "Expecting amount of comparison(s)");
+    })
+    .then(function () {
+        console.log('Clicking "Run all"');
+
+        page.evaluate(runAll);
+        return waitFor(regressionTestToExecute(page));
+    })
+    .then(function () {
+        var comparisonCount = page.evaluate(getComparisonCount);
+
+        assertEquals(comparisonCount, 2, "Expecting amount of comparison(s)");
+    })
     .then(function () {
         console.log('Smoke test successful');
         phantom.exit();
