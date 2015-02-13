cburgmer/csscritic

commit 83fbd76721fa813fcc6a11db77437cb368799fc5

Provide browser render errors on console to aid debugging

src/browser/browserRenderer.js
@@ -20,6 +20,10 @@ csscriticLib.browserRenderer = function (util, jobQueue, rasterizeHTML) {
         });
     };
 
+    var logErrorToConsole = function (e) {
+        console.error(e);
+    };
+
     var doRenderHtml = function (parameters) {
         var drawOptions = {
                 cache: 'repeated',
@@ -44,7 +48,7 @@ csscriticLib.browserRenderer = function (util, jobQueue, rasterizeHTML) {
                 image: result.image,
                 errors: renderErrors
             };
-        });
+        }, logErrorToConsole);
     };
 
     var enqueueRenderHtmlJob = function (parameters) {

commit a9da8f100248eafac8c2c6d0612c516e5bc332f4

Fix error handling in browserRenderer broken in 83fbd76721fa

src/browser/browserRenderer.js
@@ -22,6 +22,8 @@ csscriticLib.browserRenderer = function (util, jobQueue, rasterizeHTML) {
 
     var logErrorToConsole = function (e) {
         console.error(e);
+
+        throw e;
     };
 
     var doRenderHtml = function (parameters) {

test/specs/browser/browserRendererSpec.js
@@ -46,6 +46,10 @@ describe("Browser renderer", function () {
             }));
         };
 
+        var setUpRasterizeHtmlToFail = function () {
+            spyOn(rasterizeHTML, "drawHTML").and.returnValue(testHelper.failedPromise());
+        };
+
         ifNotInPhantomIt("should draw the html page if url is not an image, disable caching and execute JavaScript", function (done) {
             var theUrl = "the url",
                 theHtml = "some html";
@@ -77,7 +81,7 @@ describe("Browser renderer", function () {
         });
 
         ifNotInPhantomIt("should call the error handler if a page could not be rendered", function (done) {
-            spyOn(rasterizeHTML, "drawHTML").and.returnValue(testHelper.failedPromise());
+            setUpRasterizeHtmlToFail();
 
             browserRenderer.render({
                 url: testHelper.fixture("pageUnderTest.html"),