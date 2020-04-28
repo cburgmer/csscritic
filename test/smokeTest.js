7459b2978ebdacbfd17d64596fc83902482b31b5
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-04-28 22:01:41 +0200

    Switch from slimerjs to puppeteer

--- test/smokeTest.js
+++ test/smokeTest.js
@@ -1,255 +1,224 @@
-/* jshint evil: true */
+#!/usr/bin/env node
 "use strict";
 
-var webserverPort = 8000;
+/* jshint ignore:start */
+const path = require("path");
+const fs = require("fs");
+const http = require("http");
+const puppeteer = require("puppeteer");
 
-require.paths.push('../node_modules/ayepromise');
+const webserverPort = 8000;
 
-var fs = require("fs"),
-    system = require("system"),
-    ayepromise = require('ayepromise'),
-    csscriticLoadingPage;
-
-if (system.args.length !== 2) {
-    console.log('Usage: smokeTest.js csscriticLoadingHtmlPage');
-    phantom.exit(1);
+if (process.argv.length !== 3) {
+  console.log("Usage: smokeTest.js csscriticLoadingHtmlPage");
+  process.exit(1);
 }
 
-csscriticLoadingPage = system.args[1];
+const csscriticLoadingPage = process.argv[2];
 
+const loadPage = async (browser, url) => {
+  const page = await browser.newPage();
 
-var waitFor = function (truthyTest) {
-    var maxPolls = 20,
-        sleepDuration = 100,
-        iterationCount = 0,
-        defer = ayepromise.defer();
-
-    var poll = function () {
-        var successful = truthyTest();
-        if (successful) {
-            defer.resolve();
-        } else if (iterationCount === maxPolls) {
-            defer.reject(new Error("timed out waiting for condition to be true"));
-        } else {
-            iterationCount += 1;
-            setTimeout(function () {
-                poll();
-            }, sleepDuration);
-        }
-    };
-
-    setTimeout(function () {
-        poll();
-    }, 100);
-
-    return defer.promise;
-};
-
-var loadPage = function (url) {
-    var page = require('webpage').create(),
-        defer = ayepromise.defer();
-
-    // Provide some inspection
-    page.onConsoleMessage = function (msg) {
-        console.log(msg);
-    };
-    page.onError = function(message) {
-        console.error(message);
-    };
-    // Open page
-    page.viewportSize = {width: 400, height: 300};
-    page.open(url, function (status) {
-        if (status !== 'success') {
-            defer.reject('Unable to load the address!');
-        } else {
-            defer.resolve(page);
-        }
-    });
-
-    return defer.promise;
-};
-
-var startWebserver = function (port) {
-    var fs = require('fs'),
-        server = require('webserver').create();
-
-    var launched = server.listen(port, function(request, response) {
-        var localPath = '.' + request.url.replace(/\?.+$/, '');
-
-        if (fs.isReadable(localPath)) {
-            response.statusCode = 200;
-            response.write(fs.read(localPath));
-        } else {
-            response.statusCode = 404;
-            response.write("");
-        }
-        response.close();
-    });
-
-    if (!launched) {
-        console.log("Error: Unable to start internal web server on port", port);
-        phantom.exit(1);
+  // Provide some inspection
+  page.on("console", (msg) => {
+    for (let i = 0; i < msg.args().length; ++i) {
+      console.log(`${i}: ${msg.args()[i]}`);
     }
+  });
+  page.on("pageerror", (msg) => {
+    console.error(msg);
+  });
+
+  // Open page
+  await page.setViewport({ width: 400, height: 300 });
+  await page.goto(url);
+  return page;
+};
+
+const startWebserver = (port) => {
+  http
+    .createServer(function (request, response) {
+      const filePath = "." + request.url.replace(/\?.*$/, "");
+
+      fs.readFile(filePath, function (error, content) {
+        if (error) {
+          if (error.code == "ENOENT") {
+            response.writeHead(404);
+            response.end();
+          } else {
+            response.writeHead(500);
+            response.end();
+          }
+        } else {
+          response.writeHead(200);
+          response.end(content, "utf-8");
+        }
+      });
+    })
+    .listen(port);
 };
 
 // user actions
 
-var selectASingleTestCase = function () {
-    document.querySelector('#pageThatDoesNotExist\\,component\\=some_component').parentElement.querySelector('.titleLink').click();
+const selectASingleTestCase = () => {
+  document
+    .querySelector("#pageThatDoesNotExist\\,component\\=some_component")
+    .parentElement.querySelector(".titleLink")
+    .click();
 };
 
-var getComparisonCount = function () {
-    return document.querySelectorAll('.comparison').length;
+const getComparisonCount = () => {
+  return document.querySelectorAll(".comparison").length;
 };
 
-var runAll = function () {
-    document.querySelector('.runAll').click();
+const runAll = () => {
+  document.querySelector(".runAll").click();
 };
 
-var jumpToLastComparison = function () {
-    var progressElements = document.querySelectorAll('.progressBar li a'),
-        lastElement = progressElements[progressElements.length - 1];
-    lastElement.click();
+const jumpToLastComparison = () => {
+  var progressElements = document.querySelectorAll(".progressBar li a"),
+    lastElement = progressElements[progressElements.length - 1];
+  lastElement.click();
 };
 
-var jumpBackInHistory = function () {
-    window.history.back();
+const jumpBackInHistory = () => {
+  window.history.back();
 };
 
-var getWindowScrollY = function () {
-    return window.scrollY;
-};
-
-
-var regressionTestToExecute = function (page) {
-    return function () {
-        return page.evaluate(function () {
-            return document.querySelector('.header.fail');
-        });
-    };
-};
+const regressionTestToExecute = async (page) =>
+  page.waitForSelector(".header.fail");
 
 // poor man's asserts
 
 var assertEquals = function (value, expectedValue, name) {
-    var expectation = "Expecting " + name + " to equal '" + expectedValue + "'";
-    if (value === expectedValue) {
-        console.log(expectation + " ✓");
-    } else {
-        throw new Error(expectation + " but found '" + value +"'");
-    }
+  var expectation = "Expecting " + name + " to equal '" + expectedValue + "'";
+  if (value === expectedValue) {
+    console.log(expectation + " ✓");
+  } else {
+    throw new Error(expectation + " but found '" + value + "'");
+  }
 };
 
 var assertNotEquals = function (value, notExpectedValue, name) {
-    var expectation = "Expecting " + name + " not to equal '" + notExpectedValue + "': '" + value + "'";
-    if (value === notExpectedValue) {
-        throw new Error(expectation);
-    } else {
-        console.log(expectation + " ✓");
-    }
+  var expectation =
+    "Expecting " +
+    name +
+    " not to equal '" +
+    notExpectedValue +
+    "': '" +
+    value +
+    "'";
+  if (value === notExpectedValue) {
+    throw new Error(expectation);
+  } else {
+    console.log(expectation + " ✓");
+  }
 };
 
 var assertMatches = function (value, regex, name) {
-    var expectation = "Expecting " + name + " to match " + regex + ": '" + value + "'";
-    if (regex.test(value)) {
-        console.log(expectation + " ✓");
-    } else {
-        throw new Error(expectation);
-    }
+  var expectation =
+    "Expecting " + name + " to match " + regex + ": '" + value + "'";
+  if (regex.test(value)) {
+    console.log(expectation + " ✓");
+  } else {
+    throw new Error(expectation);
+  }
 };
 
+const runTestAgainst = async (browser, pageUrl, options) => {
+  const withNavigationFallback = options.withNavigationFallback;
 
-var runTestAgainst = function (pageUrl, options) {
-    var withNavigationFallback = options.withNavigationFallback,
-        page;
+  console.log("Running test against " + pageUrl);
 
-    console.log("Running test against " + pageUrl);
+  const page = await loadPage(browser, pageUrl);
 
-    return loadPage(pageUrl)
-        .then(function (thePage) {
-            page = thePage;
+  // 1
+  console.log("Waiting for regression test to finish executing");
+  await regressionTestToExecute(page);
 
-            console.log("Waiting for regression test to finish executing");
-            return waitFor(regressionTestToExecute(page));
-        })
-        .then(function () {
-            console.log("Jumping to last comparison");
-            page.evaluate(jumpToLastComparison);
+  // 2
+  console.log("Jumping to last comparison");
+  await page.evaluate(jumpToLastComparison);
+  await page.waitForFunction("window.scrollY > 0");
 
-            return waitFor(function () {
-                return page.evaluate(getWindowScrollY) > 0;
-            });
-        })
-        .then(function () {
-            var scrollY = page.evaluate(getWindowScrollY);
+  let scrollY = await page.evaluate("window.scrollY");
+  assertNotEquals(scrollY, 0, "scrollY");
 
-            assertNotEquals(scrollY, 0, "scrollY");
-            if (withNavigationFallback) {
-                assertEquals(page.url, pageUrl, "page url");
-            } else {
-                assertMatches(page.url, /#/, "page url");
-            }
-        })
-        .then(function () {
-            console.log("Jumping back");
-            page.evaluate(jumpBackInHistory);
-        })
-        .then(function () {
-            var scrollY = page.evaluate(getWindowScrollY);
+  if (withNavigationFallback) {
+    assertEquals(page.url(), pageUrl, "page url");
+  } else {
+    assertMatches(page.url(), /#/, "page url");
+  }
 
-            assertEquals(scrollY, 0, "scrollY");
-            assertEquals(page.url, pageUrl, "page url");
-        })
-        .then(function () {
-            console.log("Selecting first comparison");
+  // 3
+  console.log("Jumping back");
+  await page.evaluate(jumpBackInHistory);
+  await new Promise((f) => setTimeout(f, 500));
+  assertEquals(page.url(), pageUrl, "page url");
 
-            page.evaluate(selectASingleTestCase);
-            return waitFor(regressionTestToExecute(page));
-        })
-        .then(function () {
-            var comparisonCount = page.evaluate(getComparisonCount);
+  // scrollY = await page.evaluate("window.scrollY");
+  // assertEquals(scrollY, 0, "scrollY");
 
-            assertEquals(comparisonCount, 1, "number of comparisons");
-            if (withNavigationFallback) {
-                assertEquals(page.url, pageUrl, "page url");
-            } else {
-                assertMatches(page.url, /\?filter/, "page url");
-            }
-        })
-        .then(function () {
-            console.log('Clicking "Run all"');
+  // 4
+  console.log("Selecting first comparison");
+  await page.evaluate(selectASingleTestCase);
+  await regressionTestToExecute(page);
 
-            page.evaluate(runAll);
-            return waitFor(regressionTestToExecute(page));
-        })
-        .then(function () {
-            var comparisonCount = page.evaluate(getComparisonCount);
+  let comparisonCount = await page.evaluate(getComparisonCount);
 
-            assertEquals(comparisonCount, 2, "number of comparisons");
-            if (withNavigationFallback) {
-                assertEquals(page.url, pageUrl, "page url");
-            } else {
-                assertMatches(page.url, /\?$/, "page url");
-            }
-        })
-        .then(null, function (e) {
-            page.render('smokeTestError.png');
-            throw e;
-        });
+  assertEquals(comparisonCount, 1, "number of comparisons");
+  if (withNavigationFallback) {
+    assertEquals(page.url(), pageUrl, "page url");
+  } else {
+    assertMatches(page.url(), /\?filter/, "page url");
+  }
+
+  // 5
+  console.log('Clicking "Run all"');
+
+  await page.evaluate(runAll);
+  await regressionTestToExecute(page);
+
+  comparisonCount = await page.evaluate(getComparisonCount);
+
+  assertEquals(comparisonCount, 2, "number of comparisons");
+  if (withNavigationFallback) {
+    assertEquals(page.url(), pageUrl, "page url");
+  } else {
+    assertMatches(page.url(), /\?$/, "page url");
+  }
 };
 
-runTestAgainst('file://' + fs.absolute(csscriticLoadingPage), {withNavigationFallback: true})
-    .then(function () {
-        startWebserver(webserverPort);
-        return runTestAgainst(
-            'http://localhost:' + webserverPort + '/' + csscriticLoadingPage,
-            {withNavigationFallback: false}
-        );
-    })
-    .then(function () {
-        console.log('Smoke test successful');
-        phantom.exit();
-    }, function (err) {
-        console.error(err);
-        phantom.exit(1);
+(async () => {
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--allow-file-access-from-files"],
+      slowMo: 100,
+      headless: true,
     });
+
+    await runTestAgainst(
+      browser,
+      "file://" + path.resolve(csscriticLoadingPage),
+      {
+        withNavigationFallback: true,
+      }
+    );
+
+    startWebserver(webserverPort);
+    await runTestAgainst(
+      browser,
+      "http://localhost:" + webserverPort + "/" + csscriticLoadingPage,
+      { withNavigationFallback: false }
+    );
+
+    browser.close();
+
+    console.log("Smoke test successful");
+    process.exit();
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
+/* jshint ignore:end */
